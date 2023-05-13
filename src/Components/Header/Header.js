714b01d41c1266bba832971418191f7fcf0dd82c
NamwiseGamgee <akram.nayem01@gmail.com>
2023-05-13 16:16:06 +0600

    made the header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <div className='header'>
+                <div>
+                    <h4>SmartDumpCafe</h4>
+                </div>
+                <div>
+                    <img className='dp' src="girl.jpg" alt="" />
+                </div>
+            </div>
+            <hr />
+        </div>
+    );
+};
+
+export default Header;
